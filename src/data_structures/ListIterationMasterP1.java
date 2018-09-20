ed5b79664b2f22ce7949012c1a709b1ca8a4efa2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-19 18:26:26 -0700

    ListLiterationIP Computer crashes and work was not saved

--- src/data_structures/ListIterationMasterP1.java
+++ src/data_structures/ListIterationMasterP1.java
@@ -3,17 +3,26 @@ package data_structures;
 import java.util.ArrayList;
 
 public class ListIterationMasterP1 {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
+
+		songs.add(new Song("cold"));
+		songs.add(new Song("hot"));
+
+	}
+
+	static ArrayList<Song> songs = new ArrayList<>();
+
 }
 
 class Song {
-double getDuration(){
-	return 0;
-	
-}
-}
+	String songName;
 
-ArrayList<Song> songs = new ArrayList<>();
+	Song(String songName) {
+		this.songName = songName;
+	}
 
+	double getDuration() {
+		return 0.5;
+
+	}
 }
--- src/data_structures/ListIterationP2.java
+++ src/data_structures/ListIterationP2.java
@@ -6,23 +6,33 @@ public class ListIterationP2 {
 
 	public static void main(String[] args) {
 
+		classmates.add(new Student(10, "sherlin"));
+
 	}
 
-	class Student {
-		Student(int iq, String name) {
+	static ArrayList<ListIterationP2> nerds = new ArrayList<ListIterationP2>();
+	static ArrayList<Student> classmates = new ArrayList<Student>();
 
-		}
+}
 
-		int subjectiveIntelligenceLevel;
-		String name;
+class Student {
+	int iq;
+	String name;
+
+	Student(int iq, String name) {
+		this.name = name;
+		this.iq = iq;
 	}
 
-	class Classroom {
-		int getAverageIQ() {
+	int subjectiveIntelligenceLevel;
 
-			return 0;
-		}
+}
 
-		ArrayList<ListIterationP2> nerds = new ArrayList<ListIterationP2>();
+class Classroom {
+	int getAverageIQ() {
+
+		return 0;
+	
 	}
+
 }
